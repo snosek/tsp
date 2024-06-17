7e42689c387dd37b21f28a0678c5f6cc5a8611ac
Stefan Nosek <stefan.nosek1@gmail.com>
2024-06-17 10:38:58 +0200

    preorder przeniesione

--- Graph.py
+++ Graph.py
@@ -11,49 +11,15 @@ class Graph:
 		self.graph = graph
 		self.weighted = False
 
-	# dict initializer
-	@classmethod
-	def from_dict(cls, graph):
-		return cls(graph)
-
-	# array initializer
-	@classmethod
-	def from_array(cls, graph: np.array, nodes: list = None):
-		if nodes is None:
-			nodes = [*range(1, len(graph) + 1)]
-		return cls.from_dict(
-			cls._array_to_dict(graph, nodes)
-		)
-
-	@staticmethod
-	def from_edges(filename: str, directed = 0):
-		"""
-		Generates a graphs from a text file, where each line defines one edge.\n
-		Filename is a file path.
-		"""
-		graph = Graph()
-		file = open(filename, "r")
-		for line in file:
-			words = line.strip().split()
-			if len(words) == 1:
-				graph.add_node(words[0])
-			elif len(words) == 2: # two words -- unweighed graph
-				if directed:
-					graph.add_arc([words[0], words[1]])
-				else:
-					graph.add_edge([words[0], words[1]])
-			elif len(words) > 2: # more than two words - labeled graph
-				graph.weighted = True
-				if directed:
-					graph.add_arc([words[0], words[1]])
-					graph.graph[words[0]][-1] = (words[1], words[2]) # process label
-				else:
-					graph.add_edge([words[0], words[1]])
-					graph.graph[words[0]][-1] = (words[1], words[2])
-					graph.graph[words[1]][-1] = (words[0], words[2])
-		file.close()
-		return graph
-
+	def __str__(self):
+		res = ""
+		for v in self.graph:
+			res += f"{v}:"
+			for u in self.graph[v]:
+				res += f" {u}"
+			res += "\n"
+		return res
+	
 	@staticmethod
 	def random_graph(nodes_num: int, prob: float, weighed: bool = False):
 		"""
@@ -66,17 +32,7 @@ class Graph:
 				if random() < prob:
 					weight = randint(1, 10)
 					rand_graph.add_edge([i, j], weight)
-		
 		return rand_graph
-
-	def __str__(self):
-		res = ""
-		for v in self.graph:
-			res += f"{v}:"
-			for u in self.graph[v]:
-				res += f" {u}"
-			res += "\n"
-		return res
 
 	def add_node(self, node):
 		"""
@@ -119,30 +75,6 @@ class Graph:
 		if u not in self.graph[v]:
 			self.graph[v].append((u, weight))
 
-	def _array_to_dict(arr: np.array, nodes: list) -> dict:
-		"""
-		Converts a graph in array form to a graph in dict form.
-		"""
-		res_dict = {}
-		for i, node in enumerate(nodes):
-			neighbours = [nodes[j] for j, edge in enumerate(arr[i]) if edge]
-			res_dict[node] = neighbours
-		return res_dict
-
-	def _dict_to_array(self, _dict: dict) -> np.array:
-		"""
-		Converts a graph in dict form to a graph in array form.
-		"""
-		n = len(_dict)
-		nodes = [*_dict.keys()]
-		res_arr = np.zeros(shape = (n, n), dtype=int)
-		for u,v in [
-			(nodes.index(u), nodes.index(v))
-			for u, row in _dict.items() for v in row
-		]:
-			res_arr[u][v] += 1
-		return res_arr
-
 def MinSpanningTree(graph: Graph):
 	"""
 	Algorytm Jarnika-Prima -- minimalne drzewa spinające
@@ -173,16 +105,3 @@ def MinSpanningTree(graph: Graph):
 	wtree = Graph(tree)
 	wtree.weighted = True
 	return wtree
-
-def preorder(graph: Graph, node):
-	def DFS(u):
-		for v in graph.graph[u[0]]:
-			if v[0] not in visited:
-				visited.append(v[0])
-				DFS(v)
-	visited = [node]
-	for child in graph.graph[node]:
-		if child[0] not in visited:
-			visited.append(child[0])
-			DFS(child)
-	return visited
